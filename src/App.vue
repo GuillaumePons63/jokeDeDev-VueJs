a4c45c65e76ea704a32a179d2ba1997ebf537246
Guillaumeweb <guillaume@web-63.fr>
2022-08-15 12:12:32 +0200

    bootstrap ok

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+
+</script>
+
+<template class="container">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center">
+        Salut les coquinoux
+      </div>
+
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+</style>
